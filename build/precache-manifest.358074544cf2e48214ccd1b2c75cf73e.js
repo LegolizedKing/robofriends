966a1769c28279c2c820732e7f47f04834213a02
LegolizedKing <999nevi@gmail.com>
2020-04-22 17:17:27 +0530

    robofriends web app committing..

---
+++ build/precache-manifest.358074544cf2e48214ccd1b2c75cf73e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05cde4d6417f2554a01ea98aeeaf466e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01dda8862d92b797dfaf",
+    "url": "/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "a5f31bcd182128736b5e",
+    "url": "/static/css/main.2200360e.chunk.css"
+  },
+  {
+    "revision": "01dda8862d92b797dfaf",
+    "url": "/static/js/2.a10038f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a10038f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5f31bcd182128736b5e",
+    "url": "/static/js/main.e646bea4.chunk.js"
+  },
+  {
+    "revision": "65c6441c6da975648346",
+    "url": "/static/js/runtime-main.a210e4d1.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
---
+++ src/robots.js
@@ -0,0 +1,63 @@
+export const robots= [
+{
+	id:1,
+	name:'Koshy Kurian',
+	username: 'Koshy',
+	email:'koshykurian@gmail.com'
+},
+{
+	id:2,
+	name:'Qureshi Abraham',
+	username: 'Qureshi',
+	email:'qureshiabraham@gmail.com'
+},
+{
+	id:3,
+	name:'John Honai',
+	username: 'Honai',
+	email:'johnhonai@gmail.com'
+},
+{
+	id:4,
+	name:'Ayyapan Nair',
+	username: 'Nair',
+	email:'ayyapannair@gmail.com'
+},
+{
+	id:5,
+	name:'Klaus Michaelson',
+	username: 'Klaus',
+	email:'michaelsons@gmail.com'
+},
+{
+	id:6,
+	name:'Thomas Shelby',
+	username: 'Shelby',
+	email:'thomasshelby@hotmail.com'
+},
+{
+	id:7,
+	name:'Walter White',
+	username: 'Heisenberg',
+	email:'walterwhite@gmail.com'
+},
+{
+	id:8,
+	name:'White Walker',
+	username: 'Snow King',
+	email:'gameofthrones@gmail.com'
+},
+{
+	id:9,
+	name:'Quentin Tarantino',
+	username: 'Tarantino',
+	email:'quentin@yahoo.com'
+},
+{
+	id:10,
+	name:'Nevin Sunny',
+	username: 'LegolizedKing',
+	email:'nevinsunny.mec@gmail.com'
+}
+
+];
